c0586f66ebbd6ff935b56292c01e1b893781f4da
Thomas Jost <thomas.jost45@gmail.com>
2021-03-08 14:37:13 -0800

    finishes find first duplicate in array binary search implementation

--- algorithms/bs_find_first_duplicate.py
+++ algorithms/bs_find_first_duplicate.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
 """
-A fixed point in an array "A" is an index "i" such that A[i] is equal to "i".
-
-Given an array of "n" distinct integers sorted in ascending order, write a
-function that returns a "fixed point" in the array. If there is not a
-fixed point return "None".
+Write a function that takes an array of sorted integers and a key and
+returns the index of the first occurrence of that key from the array.
+Example:
+    idx   0     1  2   3    4    5    6    7    8    9
+    A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+    target = 108
+    Returns index 3 since 108 appears for the first time at
+    index 3.
 """
+
+A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+target = 285
+
+
+def find(A, target):
+    low = 0
+    high = len(A) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if A[mid] < target:
+            low = mid + 1
+        elif A[mid] > target:
+            high = mid - 1
+        else:
+            if mid - 1 < 0:
+                return mid
+            if A[mid - 1] != target:
+                return mid
+            high = mid - 1
+    return None
+
+
+x = find(A, target)
+print(x)
