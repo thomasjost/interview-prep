4d3e59353c1eb6c4a46e06e0385038aa404a2b12
Thomas Jost <thomas.jost45@gmail.com>
2021-03-08 12:16:34 -0800

    completes BST search and check methods

--- bst/binsearch.py
+++ bst/binsearch.py
@@ -56,19 +56,63 @@ class BinarySearchTree(object):
         if data == current_node.data:
             return True
 
-    def delete(self):
-        pass
+    def inorder_print_tree(self):
+        if self.root:
+            self._inorder_print_tree(self.root)
+
+    def _inorder_print_tree(self, current_node):
+        if current_node:
+            self._inorder_print_tree(current_node.left)
+            print(str(current_node.data))
+            self._inorder_print_tree(current_node.right)
+            print(str(current_node.data))
+
+    def is_bst_satisfied(self):
+        if self.root:
+            is_satisfied = self._is_bst_satisfied(self.root, self.root.data)
+
+            if is_satisfied is None:
+                return True
+            return False
+        return True
+
+    def _is_bst_satisfied(self, current_node, data):
+        if current_node.left:
+            if data > current_node.left.data:
+                return self._is_bst_satisfied(current_node.left, current_node.left.data)
+            else:
+                return False
+
+        if current_node.right:
+            if data < current_node.right.data:
+                return self._is_bst_satisfied(current_node.right, current_node.right.data)
+            else:
+                return False
 
 
 bst = BinarySearchTree()
-bst.insert(4)
-bst.insert(2)
 bst.insert(8)
-bst.insert(5)
+bst.insert(3)
 bst.insert(10)
+bst.insert(1)
+bst.insert(6)
+bst.insert(9)
+bst.insert(11)
 
 print(bst.search(5))
 print(bst.search(8))
 print(bst.search(11))
 print(bst.search(10))
 print(bst.search(12))
+
+tree = BinarySearchTree()
+tree.root = Node(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+
+print(bst.inorder_print_tree())
+
+print(tree.inorder_print_tree())
+
+print(bst.is_bst_satisfied())
+print(tree.is_bst_satisfied())
