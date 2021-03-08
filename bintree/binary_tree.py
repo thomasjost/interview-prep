a030beb9249f052c2999ed978fe7cab2507a31ad
Thomas Jost <thomas.jost45@gmail.com>
2021-03-08 11:14:08 -0800

    adds comments to both binary_tree.py and binsearch.py

--- bintree/binary_tree.py
+++ bintree/binary_tree.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+A binary tree is a tree data structure in which each node has at most two children,
+which are referred to as the left child and the right child.
+"""
 class Queue(object):
     def __init__(self):
         self.items = []
